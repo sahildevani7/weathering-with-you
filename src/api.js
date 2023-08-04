6569921f1577ea9900d1218011be62e828ccd7aa
Sahil <sahil.devani05@gmail.com>
2023-08-05 00:35:29 +0530

    search functionality

---
+++ src/api.js
@@ -0,0 +1,10 @@
+export const geoApiOptions = {
+    method: 'GET',
+    headers: {
+        'X-RapidAPI-Key': '6ab8aa9b12mshab55a6812590561p1cb6c6jsn82a5b0c9660b',
+        'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+    }
+};
+
+export const GEO_API_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo';
+
